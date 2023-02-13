fe850f71d79fc625e0fad6952bc66b4ab11b75a3
Sergio Rodríguez Calvo <sergiorodriguezcalvo@gmail.com>
2023-02-13 19:28:48 +0100

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:counter_bloc_pattern_flutter_app/app.dart';
+import 'package:counter_bloc_pattern_flutter_app/weather_bloc_observer.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:hydrated_bloc/hydrated_bloc.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:weather_repository/weather_repository.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  Bloc.observer = const WeatherBlocObserver();
+  HydratedBloc.storage = await HydratedStorage.build(
+    storageDirectory: kIsWeb
+        ? HydratedStorage.webStorageDirectory
+        : await getTemporaryDirectory(),
+  );
+  runApp(WeatherApp(weatherRepository: WeatherRepository()));
+}
